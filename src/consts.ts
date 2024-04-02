8497d8da1a152350930c4481cf09e54b7fc445cc
Andrew Weisbeck <andrew.weisbeck@gmail.com>
2024-04-02 13:00:27 -0400

    Initial commit

---
+++ src/consts.ts
@@ -0,0 +1,70 @@
+// This is your config file, place any global data here.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+type Config = {
+  title: string;
+  description: string;
+  lang: string;
+  profile: {
+    author: string;
+    description?: string;
+  }
+}
+
+type SocialLink = {
+  icon: string;
+  friendlyName: string;
+  link: string;
+}
+
+export const siteConfig: Config = {
+  title: "Astro Theme Cody",
+  description: "",
+  lang: "en-GB",
+  profile: {
+    author: "Amy Dang",
+    description: "your bio description"
+  }
+}
+
+/** 
+  Uses https://github.com/natemoo-re/astro-icon#readme
+  Find icons @ https://icones.js.org/
+*/
+export const socialLinks: Array<SocialLink> = [
+  {
+    icon: "mdi:github",
+    friendlyName: "Github",
+    link: "https://github.com/kirontoo/astro-theme-cody",
+  },
+  {
+    icon: "mdi:linkedin",
+    friendlyName: "LinkedIn",
+    link: "#",
+  },
+  {
+    icon: "mdi:email",
+    friendlyName: "email",
+    link: "mailto:ndangamy@gmail.com",
+  },
+
+];
+
+export const NAV_LINKS: Array<{ title: string, path: string }> = [
+  {
+    title: "Home",
+    path: "/",
+  },
+  {
+    title: "About",
+    path: "/about",
+  },
+  {
+    title: "Blog",
+    path: "/blog",
+  },
+  {
+    title: "Projects",
+    path: '/projects'
+  }
+];
